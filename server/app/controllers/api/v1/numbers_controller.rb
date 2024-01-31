3d4b79c20b0bd3ad4de76cb48cc39f77f47f7d0b
Omaury Marmolejo <omaury@tango.io>
2024-01-31 16:10:13 -0600

    Add logic to numbers controller

--- server/app/controllers/api/v1/numbers_controller.rb
+++ server/app/controllers/api/v1/numbers_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::NumbersController < ApplicationController
+  def single
+    number = params[:number].to_i
+    render json: classify_number(number)
+  end
+
+  def batch
+    numbers = params[:numbers].map(&:to_i)
+    render json: numbers.map { |number| classify_number(number) }
+  end
+
+  private
+
+  def classify_number(number)
+    { number: number, type: number.even? ? 'even' : 'odd' }
+  end
 end
